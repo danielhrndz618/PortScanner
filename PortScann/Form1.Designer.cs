f170262b84685154f4249221f8e11f562ea15668
Olstertecn11 <otznund@miumg.edu.gt>
2022-10-17 17:48:47 -0600

    reading ports

---
+++ PortScann/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PortScann
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_min = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_max = new System.Windows.Forms.TextBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.txt_ports = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ip.Location = new System.Drawing.Point(44, 89);
+            this.txt_ip.Multiline = true;
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(176, 29);
+            this.txt_ip.TabIndex = 0;
+            this.txt_ip.Text = "192.168.1.5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "From";
+            // 
+            // txt_min
+            // 
+            this.txt_min.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_min.Location = new System.Drawing.Point(58, 147);
+            this.txt_min.Multiline = true;
+            this.txt_min.Name = "txt_min";
+            this.txt_min.Size = new System.Drawing.Size(41, 29);
+            this.txt_min.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To";
+            // 
+            // txt_max
+            // 
+            this.txt_max.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_max.Location = new System.Drawing.Point(154, 147);
+            this.txt_max.Multiline = true;
+            this.txt_max.Name = "txt_max";
+            this.txt_max.Size = new System.Drawing.Size(66, 29);
+            this.txt_max.TabIndex = 5;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_scan.Location = new System.Drawing.Point(24, 215);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(196, 32);
+            this.btn_scan.TabIndex = 6;
+            this.btn_scan.Text = "Scan";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // txt_ports
+            // 
+            this.txt_ports.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ports.Location = new System.Drawing.Point(320, 76);
+            this.txt_ports.Multiline = true;
+            this.txt_ports.Name = "txt_ports";
+            this.txt_ports.Size = new System.Drawing.Size(313, 171);
+            this.txt_ports.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.IndianRed;
+            this.button1.Location = new System.Drawing.Point(226, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_ports);
+            this.Controls.Add(this.btn_scan);
+            this.Controls.Add(this.txt_max);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_min);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_min;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_max;
+        private System.Windows.Forms.Button btn_scan;
+        private System.Windows.Forms.TextBox txt_ports;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PortScann/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace PortScann
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.run = true;
+        }
+        public bool run; 
+
+
+        public void clearComponents()
+        {
+            this.txt_ip.Text = "";
+            this.txt_max.Text = "";
+            this.txt_min.Text= "";
+            this.txt_ports.Clear();
+        }
+
+        private void btn_scan_Click(object sender, EventArgs e)
+        {
+            int minPort = Convert.ToInt32(txt_min.Text);
+            int maxPort = Convert.ToInt32(txt_max.Text);
+
+            for(int i = minPort; i <= maxPort; i++)
+            {
+                TcpClient tcpportScan = new TcpClient();
+                try
+                {
+                    if(this.run)
+                    {
+                        tcpportScan.Connect("localhost", i);
+                        txt_ports.AppendText("Port: " + i + " is open");
+                        txt_ports.AppendText(Environment.NewLine);
+                    }
+                    else
+                    {
+                        this.clearComponents();
+                    }
+                }
+                catch
+                {
+                    txt_ports.AppendText("Port: " + i + " is closed");
+                    txt_ports.AppendText(Environment.NewLine);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.run = false;
+        }
+    }
+}
